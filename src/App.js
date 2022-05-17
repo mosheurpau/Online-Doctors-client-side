e4620dc9b546e0fbe7078b64c3354de9bf261c0f
mosheurpau <mosheur.pau@gamil.com>
2022-05-18 01:11:46 +0600

    created user route

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import MyAppointments from "./Pages/Dashboard/MyAppointments";
 import MyReview from "./Pages/Dashboard/MyReview";
+import Users from "./Pages/Dashboard/Users";
 
 function App() {
   return (
@@ -40,6 +41,7 @@ function App() {
           <Route index element={<MyAppointments></MyAppointments>}></Route>
           <Route path="review" element={<MyReview></MyReview>}></Route>
           <Route path="history" element={<MyReview></MyReview>}></Route>
+          <Route path="users" element={<Users></Users>}></Route>
         </Route>
 
         <Route path="login" element={<Login />} />
